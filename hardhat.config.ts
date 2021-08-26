b2cca772453dba29ed9dc45eceba3ab6a77d8bef
cedoor <me@cedoor.dev>
2021-08-26 16:01:45 +0200

    refactor: merge hardhat code from reputation-service repo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,97 +1,76 @@
+import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
 import "@openzeppelin/hardhat-upgrades";
-
+import "@typechain/hardhat";
+import { config as dotenvConfig } from "dotenv";
+import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/config";
+import { NetworksUserConfig } from "hardhat/types";
+import { resolve } from "path";
+import "solidity-coverage";
 import "./tasks/accounts";
 import "./tasks/clean";
 
-import { resolve } from "path";
-
-import { config as dotenvConfig } from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-// import { NetworkUserConfig } from "hardhat/types";
-
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
-const chainIds = {
-  ganache: 1337,
-  goerli: 5,
-  hardhat: 31337,
-  kovan: 42,
-  mainnet: 1,
-  rinkeby: 4,
-  ropsten: 3,
-};
+function getNetworks(): NetworksUserConfig | undefined {
+  if (process.env.NODE_ENV === "production") {
+    const infuraApiKey = process.env.INFURA_API_KEY;
+    const mnemonic = process.env.MNEMONIC;
 
-// Ensure that we have all the environment variables we need.
-// const mnemonic = process.env.MNEMONIC;
-// if (!mnemonic) {
-//   throw new Error("Please set your MNEMONIC in a .env file");
-// }
+    if (!infuraApiKey) {
+      throw new Error("Please set your INFURA_API_KEY in a .env file");
+    }
 
-// const infuraApiKey = process.env.INFURA_API_KEY;
-// if (!infuraApiKey) {
-//   throw new Error("Please set your INFURA_API_KEY in a .env file");
-// }
+    if (!mnemonic) {
+      throw new Error("Please set MNEMONIC in a .env file");
+    }
 
-// function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
-//   const url: string = `https://${network}.infura.io/v3/${infuraApiKey}`;
-//   return {
-//     accounts: {
-//       count: 10,
-//       initialIndex: 0,
-//       mnemonic,
-//       path: "m/44'/60'/0'/0",
-//     },
-//     chainId: chainIds[network],
-//     url,
-//   };
-// }
+    return {
+      ropsten: {
+        url: "https://ropsten.infura.io/v3/" + infuraApiKey,
+        chainId: 3,
+        accounts: {
+          count: 10,
+          mnemonic,
+        },
+      },
+      kovan: {
+        url: "https://kovan.infura.io/v3/" + infuraApiKey,
+        chainId: 42,
+        accounts: {
+          count: 10,
+          mnemonic,
+        },
+      },
+      arbitrum: {
+        url: "https://arb1.arbitrum.io/rpc",
+        chainId: 42161,
+        accounts: {
+          count: 10,
+          mnemonic,
+        },
+      },
+    };
+  }
+}
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: process.env.DEFAULT_NETWORK || "hardhat",
+  networks: getNetworks(),
+  solidity: {
+    version: "0.8.0",
+  },
   gasReporter: {
     currency: "USD",
     enabled: process.env.REPORT_GAS ? true : false,
-    excludeContracts: [],
-    src: "./contracts",
-  },
-  networks: {
-    hardhat: {
-      chainId: chainIds.hardhat,
-    },
-    // goerli: createTestnetConfig("goerli"),
-    // kovan: createTestnetConfig("kovan"),
-    // rinkeby: createTestnetConfig("rinkeby"),
-    // ropsten: createTestnetConfig("ropsten"),
-  },
-  paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: "0.8.0",
-    settings: {
-      metadata: {
-        // Not including the metadata hash
-        // https://github.com/paulrberg/solidity-template/issues/31
-        bytecodeHash: "none",
-      },
-      // You should disable the optimizer when debugging
-      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-      optimizer: {
-        enabled: true,
-        runs: 800,
-      },
-    },
   },
   typechain: {
     outDir: "typechain",
     target: "ethers-v5",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
---
+++ tasks/deploy-reputation-badge.ts
@@ -0,0 +1,22 @@
+import { task } from "hardhat/config";
+import { ReputationBadge } from "../typechain";
+
+task("deploy:reputation-badge", "Deploy a ReputationBadge contract")
+  .addParam("name", "The name of the token")
+  .addParam("symbol", "The symbol of the token")
+  .setAction(async ({ name, symbol }, { ethers, upgrades }) => {
+    const [signer] = await ethers.getSigners();
+    const ReputationBadgeFactory = await ethers.getContractFactory("ReputationBadge");
+
+    // Unfortunately the first signer is used to deploy and there is no option to change that
+    // See https://github.com/OpenZeppelin/openzeppelin-upgrades/issues/271
+    const reputationBadge: ReputationBadge = (await upgrades.deployProxy(ReputationBadgeFactory, [
+      name,
+      symbol,
+      signer.address,
+    ])) as ReputationBadge;
+
+    await reputationBadge.deployed();
+
+    console.log(`The ReputationBadge contract has been deployed to the address: ${reputationBadge.address}`);
+  });
