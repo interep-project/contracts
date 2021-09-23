29a94341fec969f4d8ae858d85d69c2a7ce30783
cedoor <me@cedoor.dev>
2021-09-23 18:13:49 +0200

    chore: deploy reddit ReputationBadge contract on kovan

--- scripts/deploy-mocks.ts
+++ scripts/deploy-mocks.ts
@@ -3,11 +3,15 @@ import { run } from "hardhat"
 async function main(): Promise<void> {
     await run("deploy:reputation-badge", {
         name: "InterRep Twitter Badge",
-        symbol: "iTW"
+        symbol: "iTWITTER"
     })
     await run("deploy:reputation-badge", {
         name: "InterRep Github Badge",
-        symbol: "iGH"
+        symbol: "iGITHUB"
+    })
+    await run("deploy:reputation-badge", {
+        name: "InterRep Reddit Badge",
+        symbol: "iREDDIT"
     })
     await run("deploy:interrep-groups")
 }
