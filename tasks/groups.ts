403cfca624b882e89fcd109cee1404cc550bedd0
cedoor <me@cedoor.dev>
2021-11-10 17:23:42 +0100

    feat: add merkle tree depth env variable

--- tasks/groups.ts
+++ tasks/groups.ts
@@ -1,9 +1,15 @@
 import { getOAuthProviders, getReputationLevels } from "@interrep/reputation-criteria"
+import { config as dotenvConfig } from "dotenv"
 import { task, types } from "hardhat/config"
+import { resolve } from "path"
+
+dotenvConfig({ path: resolve(__dirname, "../.env") })
+
+const depth = Number(process.env.MERKLE_TREE_DEPTH) || 16
 
 task("groups", "Create the InterRep default groups")
     .addParam<string>("address", "The address of the contract")
-    .addParam<number>("depth", "The depth of the trees", undefined, types.int)
+    .addOptionalParam<number>("depth", "The depth of the trees", depth, types.int)
     .setAction(async ({ address, depth }, { ethers }): Promise<void> => {
         const contract = await ethers.getContractAt("Groups", address)
         const admin = await contract.signer.getAddress()
