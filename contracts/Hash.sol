05acfcb9d48250f5567454184110d27d399a4e8d
cedoor <me@cedoor.dev>
2021-11-11 12:37:15 +0100

    refactor: remove unused var

--- contracts/Hash.sol
+++ contracts/Hash.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 library Hash {
-    function poseidon(uint256[2] memory input) public pure returns (uint256) {}
+    function poseidon(uint256[2] memory) public pure returns (uint256) {}
 }
